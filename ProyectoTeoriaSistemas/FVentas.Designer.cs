dba35ced244c03086266b8cb641887ce32b6b636
Geovanny <geovanny.alcon2@gmail.com>
2025-02-08 10:50:54 -0600

    Modulo de Ventas

--- ProyectoTeoriaSistemas/FVentas.Designer.cs
+++ ProyectoTeoriaSistemas/FVentas.Designer.cs
@@ -34,12 +34,7 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.label6 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
             this.Eliminar = new System.Windows.Forms.Button();
             this.Editar = new System.Windows.Forms.Button();
             this.Agregar = new System.Windows.Forms.Button();
@@ -55,17 +50,17 @@
             // 
             // lblNotificacion
             // 
-            this.lblNotificacion.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lblNotificacion.Dock = System.Windows.Forms.DockStyle.Top;
             this.lblNotificacion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.lblNotificacion.Font = new System.Drawing.Font("Century Gothic", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblNotificacion.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(53)))), ((int)(((byte)(85)))));
-            this.lblNotificacion.Location = new System.Drawing.Point(0, 498);
+            this.lblNotificacion.Location = new System.Drawing.Point(0, 0);
+            this.lblNotificacion.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblNotificacion.Name = "lblNotificacion";
-            this.lblNotificacion.Size = new System.Drawing.Size(1344, 119);
+            this.lblNotificacion.Size = new System.Drawing.Size(1215, 84);
             this.lblNotificacion.TabIndex = 26;
-            this.lblNotificacion.Text = "Notificacion";
+            this.lblNotificacion.Text = "Ventas";
             this.lblNotificacion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.lblNotificacion.Visible = false;
             // 
             // groupBox1
             // 
@@ -73,28 +68,27 @@
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label4);
-            this.groupBox1.Controls.Add(this.label5);
-            this.groupBox1.Controls.Add(this.textBox5);
-            this.groupBox1.Controls.Add(this.label6);
-            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.Agregar);
             this.groupBox1.Controls.Add(this.textBox2);
-            this.groupBox1.Controls.Add(this.textBox3);
             this.groupBox1.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(40, 46);
+            this.groupBox1.Location = new System.Drawing.Point(29, 90);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(570, 291);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(1149, 215);
             this.groupBox1.TabIndex = 25;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Nuevo Producto";
+            this.groupBox1.Text = "Venta";
             // 
             // textBox1
             // 
             this.textBox1.BackColor = System.Drawing.Color.Lavender;
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox1.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(172, 50);
+            this.textBox1.Location = new System.Drawing.Point(164, 55);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(352, 20);
+            this.textBox1.Size = new System.Drawing.Size(440, 20);
             this.textBox1.TabIndex = 10;
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
@@ -104,11 +98,12 @@
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(53)))), ((int)(((byte)(85)))));
-            this.label2.Location = new System.Drawing.Point(28, 40);
+            this.label2.Location = new System.Drawing.Point(35, 48);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(33, 27);
+            this.label2.Size = new System.Drawing.Size(121, 27);
             this.label2.TabIndex = 5;
-            this.label2.Text = "Id";
+            this.label2.Text = "Cliente(*)";
             // 
             // label3
             // 
@@ -116,11 +111,12 @@
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(53)))), ((int)(((byte)(85)))));
-            this.label3.Location = new System.Drawing.Point(28, 83);
+            this.label3.Location = new System.Drawing.Point(649, 55);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(103, 27);
+            this.label3.Size = new System.Drawing.Size(82, 27);
             this.label3.TabIndex = 6;
-            this.label3.Text = "Nombre";
+            this.label3.Text = "Fecha";
             // 
             // label4
             // 
@@ -128,89 +124,36 @@
             this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(53)))), ((int)(((byte)(85)))));
-            this.label4.Location = new System.Drawing.Point(28, 129);
+            this.label4.Location = new System.Drawing.Point(35, 87);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(85, 27);
+            this.label4.Size = new System.Drawing.Size(236, 27);
             this.label4.TabIndex = 7;
-            this.label4.Text = "Marca";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.BackColor = System.Drawing.Color.Transparent;
-            this.label5.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(53)))), ((int)(((byte)(85)))));
-            this.label5.Location = new System.Drawing.Point(28, 178);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(83, 27);
-            this.label5.TabIndex = 8;
-            this.label5.Text = "Precio";
-            // 
-            // textBox5
-            // 
-            this.textBox5.BackColor = System.Drawing.Color.Lavender;
-            this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox5.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox5.Location = new System.Drawing.Point(172, 231);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(352, 20);
-            this.textBox5.TabIndex = 14;
-            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.BackColor = System.Drawing.Color.Transparent;
-            this.label6.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(53)))), ((int)(((byte)(85)))));
-            this.label6.Location = new System.Drawing.Point(28, 223);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(117, 27);
-            this.label6.TabIndex = 9;
-            this.label6.Text = "Cantidad";
-            // 
-            // textBox4
-            // 
-            this.textBox4.BackColor = System.Drawing.Color.Lavender;
-            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox4.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox4.Location = new System.Drawing.Point(172, 186);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(352, 20);
-            this.textBox4.TabIndex = 13;
-            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.label4.Text = "Tipo Comprobate(*)";
             // 
             // textBox2
             // 
             this.textBox2.BackColor = System.Drawing.Color.Lavender;
             this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox2.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(172, 91);
+            this.textBox2.Location = new System.Drawing.Point(739, 62);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(352, 20);
+            this.textBox2.Size = new System.Drawing.Size(304, 20);
             this.textBox2.TabIndex = 11;
             this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // textBox3
-            // 
-            this.textBox3.BackColor = System.Drawing.Color.Lavender;
-            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox3.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.Location = new System.Drawing.Point(172, 137);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(352, 20);
-            this.textBox3.TabIndex = 12;
-            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // Eliminar
             // 
             this.Eliminar.BackColor = System.Drawing.Color.Red;
+            this.Eliminar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.Eliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.Eliminar.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Eliminar.ForeColor = System.Drawing.Color.White;
-            this.Eliminar.Location = new System.Drawing.Point(329, 356);
+            this.Eliminar.Location = new System.Drawing.Point(978, 501);
+            this.Eliminar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Eliminar.Name = "Eliminar";
-            this.Eliminar.Size = new System.Drawing.Size(180, 51);
+            this.Eliminar.Size = new System.Drawing.Size(200, 60);
             this.Eliminar.TabIndex = 24;
             this.Eliminar.Text = "Eliminar";
             this.Eliminar.UseVisualStyleBackColor = false;
@@ -221,9 +164,10 @@
             this.Editar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.Editar.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Editar.ForeColor = System.Drawing.Color.White;
-            this.Editar.Location = new System.Drawing.Point(1139, 437);
+            this.Editar.Location = new System.Drawing.Point(978, 408);
+            this.Editar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Editar.Name = "Editar";
-            this.Editar.Size = new System.Drawing.Size(180, 51);
+            this.Editar.Size = new System.Drawing.Size(200, 60);
             this.Editar.TabIndex = 23;
             this.Editar.Text = "Editar";
             this.Editar.UseVisualStyleBackColor = false;
@@ -231,14 +175,16 @@
             // Agregar
             // 
             this.Agregar.BackColor = System.Drawing.Color.SkyBlue;
+            this.Agregar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.Agregar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.Agregar.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Agregar.ForeColor = System.Drawing.Color.White;
-            this.Agregar.Location = new System.Drawing.Point(128, 356);
+            this.Agregar.Location = new System.Drawing.Point(71, 131);
+            this.Agregar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Agregar.Name = "Agregar";
-            this.Agregar.Size = new System.Drawing.Size(180, 51);
+            this.Agregar.Size = new System.Drawing.Size(200, 60);
             this.Agregar.TabIndex = 22;
-            this.Agregar.Text = "Agregar";
+            this.Agregar.Text = "Agregar Articulo";
             this.Agregar.UseVisualStyleBackColor = false;
             // 
             // dataGridView1
@@ -252,11 +198,12 @@
             this.Precio,
             this.Cantidad});
             this.dataGridView1.GridColor = System.Drawing.SystemColors.ActiveCaption;
-            this.dataGridView1.Location = new System.Drawing.Point(626, 54);
+            this.dataGridView1.Location = new System.Drawing.Point(100, 325);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(693, 371);
+            this.dataGridView1.Size = new System.Drawing.Size(839, 366);
             this.dataGridView1.TabIndex = 21;
             // 
             // ID
@@ -296,15 +243,16 @@
             // 
             // FVentas
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1344, 617);
+            this.ClientSize = new System.Drawing.Size(1215, 722);
             this.Controls.Add(this.lblNotificacion);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.Eliminar);
             this.Controls.Add(this.Editar);
-            this.Controls.Add(this.Agregar);
             this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FVentas";
             this.Text = "FVentas";
             this.groupBox1.ResumeLayout(false);
@@ -322,12 +270,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Button Eliminar;
         private System.Windows.Forms.Button Editar;
         private System.Windows.Forms.Button Agregar;
