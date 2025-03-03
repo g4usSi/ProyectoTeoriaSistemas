af4caa659ab2ff272fbe1bfca572b93979acd757
Jeidy Saquic <jeidysaquic201932027@cunoc.edu.gt>
2025-03-02 18:22:58 -0600

    Actualizacion

--- ProyectoTeoriaSistemas/CodigoFuente/Planilla.cs
+++ ProyectoTeoriaSistemas/CodigoFuente/Planilla.cs
@@ -1,44 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-//Esta es la planilla de empleado, falta la planilla general
 
 namespace ProyectoTeoriaSistemas.CodigoFuente
-{
-    //Esta es la que realmente tenes que usar OwO
+{    // Esta es la clase que tienes que usar 
     public class Planilla
     {
-        //Este objeto solo almacena los datos de los empleados, para acceder a el, tienes que llamar sus atributos, no se si esta bien en la clase estatica
-        public EmpleadoDatos Empleado { get; set; }  // Datos del empleado
-        public decimal SueldoOrdinario { get; set; } // Sueldo base sin extras
-        public int HorasExtras { get; set; }        // Cantidad de horas extra trabajadas
-        public decimal Comisiones { get; set; }     // Comisiones ganadas
-        public decimal Bonificaciones { get; set; } // Bonos adicionales
-        public decimal IGSS { get; set; }           // Descuento del IGSS
-        public decimal Prestamos { get; set; }      // Descuento por préstamos
+        //Este objeto solo almacena los datos de los empleados, para acceder a el,
+        //tienes que llamar sus atributos, no se si esta bien en la clase estatica
+        public EmpleadoDatos Empleado { get; set; }
+        public decimal SueldoOrdinario { get; set; }
+        public int HorasExtras { get; set; }
+        public decimal Comisiones { get; set; }
+        public decimal Bonificaciones { get; set; }
+        public decimal IGSS { get; set; }
+        public decimal Prestamos { get; set; }
 
-        // Cálculo del pago por horas extras (asumiendo que se paga 1.5x el sueldo normal por hora extra)
         public decimal PagoHorasExtras => (SueldoOrdinario / 160) * 1.5m * HorasExtras;
-
-        // Total Devengado = Sueldo Ordinario + Horas Extras + Comisiones + Bonificaciones
         public decimal TotalDevengado => SueldoOrdinario + PagoHorasExtras + Comisiones + Bonificaciones;
-
-        // Total de Descuentos = IGSS + Préstamos
         public decimal TotalDescuentos => IGSS + Prestamos;
-
-        // Sueldo Neto = Total Devengado - Total de Descuentos
         public decimal SueldoNeto => TotalDevengado - TotalDescuentos;
     }
 
-    //Omite la Existencia de este, es adorno
     public class EmpleadoDatos
     {
-        public string DPI { get; set; }         // Identificación única del empleado
-        public string Nombre { get; set; }      // Nombre completo
-        public string Cargo { get; set; }       // Puesto de trabajo
-        public decimal Sueldo { get; set; }     // Sueldo base
-        public DateTime FechaNacimiento { get; set; } // Fecha de nacimiento del empleado
+        public string DPI { get; set; }
+        public string Nombre { get; set; }
+        public string Cargo { get; set; }
+        public decimal Sueldo { get; set; }
+        public DateTime FechaNacimiento { get; set; }
     }
 }
