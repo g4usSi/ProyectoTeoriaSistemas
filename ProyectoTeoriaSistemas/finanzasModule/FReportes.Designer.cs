b3ab87cba7ba7b29a89454db896844afa7ade587
Geovanny <geovanny.alcon2@gmail.com>
2025-03-29 17:18:38 -0600

    Correccion

--- ProyectoTeoriaSistemas/finanzasModule/FReportes.Designer.cs
+++ ProyectoTeoriaSistemas/finanzasModule/FReportes.Designer.cs
@@ -29,57 +29,24 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.btnAgregarIngreso = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(30, 118);
+            this.dataGridView1.Location = new System.Drawing.Point(27, 24);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
             this.dataGridView1.Size = new System.Drawing.Size(740, 437);
             this.dataGridView1.TabIndex = 0;
             // 
-            // btnAgregarIngreso
-            // 
-            this.btnAgregarIngreso.Location = new System.Drawing.Point(30, 46);
-            this.btnAgregarIngreso.Name = "btnAgregarIngreso";
-            this.btnAgregarIngreso.Size = new System.Drawing.Size(165, 53);
-            this.btnAgregarIngreso.TabIndex = 1;
-            this.btnAgregarIngreso.Text = "Agregar Ingreso";
-            this.btnAgregarIngreso.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(799, 118);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(162, 144);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(799, 279);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(162, 144);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
             // FReportes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(991, 583);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.btnAgregarIngreso);
             this.Controls.Add(this.dataGridView1);
             this.Name = "FReportes";
             this.Text = "FReportes";
@@ -91,8 +58,5 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button btnAgregarIngreso;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
     }
 }
