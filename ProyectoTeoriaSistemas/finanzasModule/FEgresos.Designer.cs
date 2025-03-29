9077efa6cb61b18bb7a9ec58e42356d8ef0eb38b
Geovanny <geovanny.alcon2@gmail.com>
2025-03-29 17:14:55 -0600

    Estructura basica y comportamiento del modulo

--- ProyectoTeoriaSistemas/finanzasModule/FEgresos.Designer.cs
+++ ProyectoTeoriaSistemas/finanzasModule/FEgresos.Designer.cs
@@ -28,12 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnAgregarIngreso = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(799, 270);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(162, 144);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(799, 109);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 144);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btnAgregarIngreso
+            // 
+            this.btnAgregarIngreso.Location = new System.Drawing.Point(30, 37);
+            this.btnAgregarIngreso.Name = "btnAgregarIngreso";
+            this.btnAgregarIngreso.Size = new System.Drawing.Size(165, 53);
+            this.btnAgregarIngreso.TabIndex = 5;
+            this.btnAgregarIngreso.Text = "Agregar Egreso";
+            this.btnAgregarIngreso.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(30, 109);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(740, 437);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // FEgresos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(991, 583);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnAgregarIngreso);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "FEgresos";
             this.Text = "FEgresos";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnAgregarIngreso;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
