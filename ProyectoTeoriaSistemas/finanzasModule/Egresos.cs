57f4c00220ca43db73dd047cce326ac9e5bfdce6
Jeidy Saquic <jeidysaquic201932027@cunoc.edu.gt>
2025-05-03 22:49:10 -0600

    clases

---
+++ ProyectoTeoriaSistemas/finanzasModule/Egresos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoTeoriaSistemas.finanzasModule
+{
+    public class Egresos
+    {
+        public int ID { get; set; } //
+        public DateTime DateTime { get; set; }
+        public string Descripcion { get; set; }//
+        public string Total { get; set; }//
+    }
+}
---
+++ ProyectoTeoriaSistemas/finanzasModule/Ingresos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoTeoriaSistemas.finanzasModule
+{
+    public class Ingresos
+    {
+        public int ID { get; set; } //
+        public DateTime DateTime { get; set; }
+        public string Descripcion { get; set; }//
+        public string Total { get; set; }//
+       
+    }
+}
